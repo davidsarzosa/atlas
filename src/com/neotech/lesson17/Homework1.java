70bbd6f5b44b7386ad9dfa574de6fce72e96fb1e
David Sarzosa <7avids@Davids-MBP.lan>
2025-03-30 23:05:09 -0500

    Lesson17more

--- src/com/neotech/lesson17/Homework1.java
+++ src/com/neotech/lesson17/Homework1.java
@@ -1,7 +1,15 @@
 package com.neotech.lesson17;
 
 public class Homework1 {
-	//Create a String that will hold a sentence. 
-			//Write a program to get a new String without any spaces.
+	// Create a String that will hold a sentence.
+	// Write a program to get a new String without any spaces.
 
+	public static void main(String[] args) {
+
+		String Austin = "Today is sunday and we are going to run! ";
+		System.out.println(Austin);
+		System.out.println(Austin.replace(" ", ""));
+		
+
+	}
 }
--- src/com/neotech/lesson17/Homework2.java
+++ src/com/neotech/lesson17/Homework2.java
@@ -1,5 +1,7 @@
 package com.neotech.lesson17;
 
+import java.util.Scanner;
+
 public class Homework2 {
 //	Write a program that reads two parent's first names and if they expecting boy or girl?
 //    Based on the input suggests a name for a baby:
@@ -19,4 +21,39 @@ public class Homework2 {
 //		 
 //		 */
 
+	public static void main(String[] args) {
+
+		// Gets the parents names first
+
+		Scanner inputScan = new Scanner(System.in);
+
+		System.out.println("Enter the father's name: ");
+		String dad = inputScan.next();
+		System.out.println("Enter the mother's name : ");
+		String mom = inputScan.next();
+
+		// get the gender ?
+
+		String babyName = ""; // we are looking for this!
+
+		System.out.println("Are you specting a boy or a girl ? ");
+		String gender = inputScan.next();
+
+		// Calculate half lengths!
+
+		int dadHalf = dad.length() / 2;
+		int momHalf = mom.length() / 2;
+
+		if (gender.equals("boy")) {
+			babyName = dad.substring(0, dadHalf) + mom.substring(0, momHalf);
+
+		} else if (gender.equals("girl")) {
+			babyName = mom.substring(0, momHalf) + dad.substring(0, dadHalf);
+		} else {
+			System.out.println("Enter boy or girl");
+			return;
+		}
+		System.out.println("Suggested baby name: " + babyName);
+
+	}
 }
